d296d3022609ec988e24dd4818314237170e7ea4
Nick Stephens <robert.nicholas.stephens@gmail.com>
2024-01-27 23:15:41 -0600

    scrolling text

--- src/Components/Pages/AboutPage.js
+++ src/Components/Pages/AboutPage.js
@@ -1,14 +1,24 @@
+import Button from 'react-bootstrap/Button';
 
 
 export default function AboutPage() {
     return (
         <div style={{ textAlign: 'center', padding: '20px', backgroundColor: 'white' }}>
             <h1 style={{ fontSize: '24px', fontWeight: 'bold', color: 'black' }}>
-                American Truck Simulator, the greatest game of all time, does not track achievements well enough.
-                That's where the real truckers come in. We're here to help you track your progress and get that 100% completion.
+                <marquee behavior="" direction="left" loop>
+                    American Truck Simulator, the greatest game of all time, does not track achievements well enough.
+                    That's where the real truckers come in. We're here to help you track your progress and get that 100% completion.
+                </marquee>
             </h1>
             <div style={{ marginTop: '20px' }}>
-                <a href="https://github.com/your-username/your-repo" target="_blank" rel="noopener noreferrer">Click here</a> to visit our GitHub repository.
+                <Button variant="primary" href="https://github.com/BenBenBenB/truck-sim-tracker" target="_blank" rel="noopener noreferrer">
+                    Truck Sim Tracker
+                </Button>
+            </div>
+            <div style={{ marginTop: '20px' }}>
+                <Button variant="primary" href="https://github.com/BenBenBenB/truck-sim-tracker-ui" target="_blank" rel="noopener noreferrer">
+                    Truck Sim Tracker UI
+                </Button>
             </div>
         </div>
     );
